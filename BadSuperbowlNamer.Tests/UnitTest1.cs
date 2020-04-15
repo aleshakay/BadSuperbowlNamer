5701e0211c888586e1d592933c4e7b503216b22b
alesha.reed <aleshak83@gmail.com>
2020-04-14 20:49:57 -0500

    Add project files.

---
+++ BadSuperbowlNamer.Tests/UnitTest1.cs
@@ -0,0 +1,70 @@
+using System;
+using Xunit;
+
+namespace BadSuperbowlNamer.Tests
+{
+    public class int_to_roman_numerals
+    {
+        [Fact]
+        public void translating_1_results_in_I ()
+        {
+            //Arrange
+            var number = 1;
+            var expectedResult = "I";
+            var translator = new Translator();
+            //Act
+            var actualResult = translator.Translate(number);
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void translatting_5_results_in_V()
+        {
+            //Arrange
+            var number = 5;
+            var expectedResult = "V";
+            var translator = new Translator();
+            //Act
+            var actualResult = translator.Translate(number);
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void translatting_9_results_in_IX()
+        {
+            //Arrange
+            var number = 9;
+            var expectedResult = "IX";
+            var translator = new Translator();
+            //Act
+            var actualResult = translator.Translate(number);
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void translatting_12_results_in_XII()
+        {
+            //Arrange
+            var number = 12;
+            var expectedResult = "XII";
+            var translator = new Translator();
+            //Act
+            var actualResult = translator.Translate(number);
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translatting_14_results_in_XIV()
+        {
+            //Arrange
+            var number = 14;
+            var expectedResult = "XIV";
+            var translator = new Translator();
+            //Act
+            var actualResult = translator.Translate(number);
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
